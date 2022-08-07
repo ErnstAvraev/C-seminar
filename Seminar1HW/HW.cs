b743b979eb07cf46694e8779581983cfe7efc549
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-07 18:14:19 +0300

    finished task 6, started task 8

--- Seminar1HW/HW.cs
+++ Seminar1HW/HW.cs
@@ -47,3 +47,16 @@
 
 // Задача 6
 
+// Console.WriteLine("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a%2!=0){
+//     Console.WriteLine("Число нечетное");
+// }
+// else {
+//     Console.WriteLine("Число четное");
+// }
+
+// Задача 8
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine()):
