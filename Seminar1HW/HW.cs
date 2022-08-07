bc78d78e358eed5b25153a3352a1ff1b6e8bf3a9
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-07 18:01:48 +0300

    added task 4, started task 6

--- Seminar1HW/HW.cs
+++ Seminar1HW/HW.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число b: " );
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = new Random().Next(1, 100);
+// Console.WriteLine("Число а = " + a);
+// int b = new Random().Next(1, 100);
+// Console.WriteLine("Число b = "+ b);
+// int max = 0;
+
+// if (a > b) {
+//     max = a;
+// }
+// else {
+//     max = b;
+// }
+// Console.WriteLine("Максимальное число равно " + max);
+
+// Задача 4
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число b: " );
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число c: " );
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int a = new Random().Next(1, 100);
+// Console.WriteLine("Число а = " + a);
+// int b = new Random().Next(1, 100);
+// Console.WriteLine("Число b = "+ b);
+// int c = new Random().Next(1, 100);
+// Console.WriteLine("Число c = " + c);
+
+// int max = 0;
+
+// if (a>max){
+//     max=a;
+// }
+// if (b>max){
+//     max=b;
+// }
+// if (c>max){
+//     max=c;
+// }
+// Console.WriteLine("Максимальное число = "+max);
+
+// Задача 6
+
