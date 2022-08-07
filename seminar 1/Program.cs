66ff2fa35a67de009c6d64876534c0b109036409
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-07 16:28:30 +0300

    comments

--- seminar 1/Program.cs
+++ seminar 1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+// int sqrt = number * number;
+// number *= number;
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+// Console.WriteLine("Число в квадрате: " + sqrt);
+// Console.WriteLine("Число в квадрате: " + number);
+Console.WriteLine("Число в квадрате: " + sqrt1);
