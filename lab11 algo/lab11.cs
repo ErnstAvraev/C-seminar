a430e0e48a48bc86e32821dc6beb3c91d27c6197
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-09 19:42:45 +0300

    new algoritmic structures

--- lab11 algo/lab11.cs
+++ lab11 algo/lab11.cs
@@ -1,1 +1,82 @@
-﻿
+﻿// string mes1 = "", mes2 = ""; // объявление и инициализация
+// Console.WriteLine("Введите целое число ");
+
+// // ввод строки и преобразование в целое число
+
+// int n = int.Parse(Console.ReadLine());
+// if (n>=0) mes1 ="положительное";
+// else mes1 = "отрицательное";
+// if (n%2==0) mes2 = "четное";
+// else mes2 = "нечетное";
+// Console.WriteLine("Число {0} {1} {2}", n, mes1, mes2);
+// Console.ReadKey();
+
+
+
+// Random rnd = new Random(); // создаем экземпляр класса Random
+// // генерируем случайное целое число в интервале -40, 40
+// int n = rnd.Next(-40, 40);
+// // используем тернарный оператор
+// string mes1 = (n >= 0) ? "положительное" : "отрицательное";
+// string mes2 = (n % 2 == 0) ? "четное" : "нечетное";
+// Console.WriteLine("Число {0} {1} {2}", n, mes1, mes2);
+// Console.ReadKey();
+
+
+
+// string mes;
+// Console.WriteLine("Введите день недели: Пн, Вт, Ср, Чт, Пт, Сб, Вс");
+// string day = Console.ReadLine();
+// switch (day)
+// { case "Сб": mes = "Иду в гости"; break;
+// case "Вс": mes = "Отдыхаю"; break;
+// default: mes = "Работаю"; break;
+// }
+// Console.WriteLine(mes);
+// Console.ReadKey();
+
+
+
+// string myLog = "qwerty", myPas = "asdf";
+// string mes = "", mesYes = "Добро пожаловать!", mesNo = "Вход воспрещен!";
+// Console.Write("Введите логин: ");
+// string log = Console.ReadLine(); // вводим логин
+// // проверяем длину и совпадение логина
+// if (log.Length < 6) mes = "Логин очень короткий!\n" + mesNo;
+// else if (log != myLog) mes = "Неверный логин!\n" + mesNo;
+// else
+// // в случае истинности логина вводим и проверяем пароль
+// { Console.Write("Введите пароль: ");
+// string pas = Console.ReadLine(); // вводим пароль
+// mes = (pas == myPas) ? mesYes : "Неверный пароль!\n" + mesNo;
+// }
+// Console.WriteLine(mes); // выводим сообщения
+// Console.ReadKey();
+
+
+
+double A, B, res=0; bool ok=true;
+Console.Write("Введите число А: ");
+string sA = Console.ReadLine(); // ввод строки
+if (!double.TryParse(sA, out A))
+{ Console.WriteLine("Не число!"); A = 1; }; // преобразование в число
+
+Console.Write("Введите число B: ");
+string sB = Console.ReadLine();
+if (!double.TryParse(sB, out B))
+{ Console.WriteLine("Не число!"); B = 1; }
+
+Console.Write("Введите знак операции (+-*/) ");
+string op = Console.ReadLine();
+
+switch (op)
+{ case "+": res = A + B; break;
+case "-": res = A - B; break;
+case "*": res = A * B; break;
+case "/": res = A / B; break;
+default: ok = false; break;
+}
+if (ok)
+Console.WriteLine("{0} {1} {2} = {3}", A, op, B, res); //вывод
+else Console.WriteLine("Недопустимая операция");
+Console.ReadKey();
