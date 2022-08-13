c592bf7d96da772a0b553adbce68a93610bcb1d0
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-13 16:03:59 +0700

    finished HW

--- Seminar2HW/HW2.cs
+++ Seminar2HW/HW2.cs
@@ -1,1 +1,37 @@
-﻿
+﻿// Задача 10
+
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine(number);
+// Console.Write($"{number%100*1/10}");
+
+// Задача 13
+
+// int number = new Random().Next(9, 1000);
+// Console.WriteLine(number);
+
+
+// if (number<=99)
+// {
+//     Console.WriteLine("Нет третьего числа");
+// }
+// else
+// {
+//     Console.WriteLine(number%10);
+// }
+
+// Задача 15
+// Console.WriteLine("Введите цифру");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a>5 && a<8)
+// {
+//     Console.WriteLine("Выходной");
+// }
+// if (a>0 && a<6)
+// {
+//     Console.WriteLine("Будни");
+// }
+// if(a>7)
+// {
+//     Console.WriteLine("Не день недели");
+// }
