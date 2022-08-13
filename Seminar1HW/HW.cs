aeb977066b073fadfd51e3fb7c5215dc9e107dd6
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-13 08:41:02 +0300

    added some code from seminar

--- Seminar1HW/HW.cs
+++ Seminar1HW/HW.cs
@@ -56,8 +56,8 @@
 
 // Задача 6
 
-Console.WriteLine("Введите число ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
 // int a = new Random().Next(1, 100);
 // Console.WriteLine("Число a = " + a);
@@ -70,33 +70,62 @@ int a = Convert.ToInt32(Console.ReadLine());
 // {
 //     Console.WriteLine("Число нечетное");
 // }
-if (a % 2 != 0)
-{
-    Console.WriteLine("Число нечетное");
-}
-else
-{
-    Console.WriteLine("Число четное");
-}
+// if (a % 2 != 0)
+// {
+//     Console.WriteLine("Число нечетное");
+// }
+// else
+// {
+//     Console.WriteLine("Число четное");
+// }
 
 // Задача 8
+// Console.Clear();
 // Console.WriteLine("Введите число ");
 // int N = Convert.ToInt32(Console.ReadLine());
 
 // int N = new Random().Next(1, 100);
 // Console.WriteLine("Число N = " + N);
 
+// // Мое решение
+
 // int i = 0;
 
-// if (N%2!=0) 
-// {   while (i<N-1){
-//     i+=2;
-//     Console.WriteLine(i);
-// }
-// }
-// else {
-//     while (i<N) {
-//         i+=2;
+// if (N % 2 != 0)
+// {
+//     while (i < N - 1)
+//     {
+//         i += 2;
 //         Console.WriteLine(i);
+//     }
 // }
+// else
+// {
+//     while (i < N)
+//     {
+//         i += 2;
+//         Console.WriteLine(i);
+//     }
+// }
+
+// // Решение Елены 1
+
+// int i = 1;
+// while (i <= N)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write($"{i} "); //Console.Write(i + " ");
+//     }
+//     i++;
+// }
+// Console.WriteLine();
+
+// // Решение Елены 2
+
+// int k = 2;
+// while (k <= N)
+// {
+//     Console.Write($"{k} ");
+//     k += 2;
 // }
