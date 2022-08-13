721f02c8b1fc3b3511d2a5201239da095f270610
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-13 07:27:15 +0300

    all in comments

--- lab11 algo/lab11.cs
+++ lab11 algo/lab11.cs
@@ -55,28 +55,28 @@
 
 
 
-double A, B, res=0; bool ok=true;
-Console.Write("Введите число А: ");
-string sA = Console.ReadLine(); // ввод строки
-if (!double.TryParse(sA, out A))
-{ Console.WriteLine("Не число!"); A = 1; }; // преобразование в число
+// double A, B, res=0; bool ok=true;
+// Console.Write("Введите число А: ");
+// string sA = Console.ReadLine(); // ввод строки
+// if (!double.TryParse(sA, out A))
+// { Console.WriteLine("Не число!"); A = 1; }; // преобразование в число
 
-Console.Write("Введите число B: ");
-string sB = Console.ReadLine();
-if (!double.TryParse(sB, out B))
-{ Console.WriteLine("Не число!"); B = 1; }
+// Console.Write("Введите число B: ");
+// string sB = Console.ReadLine();
+// if (!double.TryParse(sB, out B))
+// { Console.WriteLine("Не число!"); B = 1; }
 
-Console.Write("Введите знак операции (+-*/) ");
-string op = Console.ReadLine();
+// Console.Write("Введите знак операции (+-*/) ");
+// string op = Console.ReadLine();
 
-switch (op)
-{ case "+": res = A + B; break;
-case "-": res = A - B; break;
-case "*": res = A * B; break;
-case "/": res = A / B; break;
-default: ok = false; break;
-}
-if (ok)
-Console.WriteLine("{0} {1} {2} = {3}", A, op, B, res); //вывод
-else Console.WriteLine("Недопустимая операция");
-Console.ReadKey();
+// switch (op)
+// { case "+": res = A + B; break;
+// case "-": res = A - B; break;
+// case "*": res = A * B; break;
+// case "/": res = A / B; break;
+// default: ok = false; break;
+// }
+// if (ok)
+// Console.WriteLine("{0} {1} {2} = {3}", A, op, B, res); //вывод
+// else Console.WriteLine("Недопустимая операция");
+// Console.ReadKey();
