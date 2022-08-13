3c3cfb1e81be6206569112127cf9ac81008ed7f5
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-13 07:28:14 +0300

    all in comments

--- lection2/lection2.cs
+++ lection2/lection2.cs
@@ -87,53 +87,53 @@
 
 // Задача с массивами и методами
 
-void FillArray(int[] collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
-}
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+// }
 
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position <count)
-    {
-        Console.WriteLine(col[position]);
-        position++;
-    }
-}
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position <count)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+// }
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
+// int IndexOf(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
 
-    while (index < count)
-    {
-        if(collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
+//     while (index < count)
+//     {
+//         if(collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
 
-int [] array = new int [10];
+// int [] array = new int [10];
 
-FillArray(array);
-array[4] = 4;
-array[6] = 4;
-PrintArray(array);
-Console.WriteLine();
+// FillArray(array);
+// array[4] = 4;
+// array[6] = 4;
+// PrintArray(array);
+// Console.WriteLine();
 
-int pos = IndexOf(array, 444);
-Console.WriteLine(pos);
+// int pos = IndexOf(array, 444);
+// Console.WriteLine(pos);
