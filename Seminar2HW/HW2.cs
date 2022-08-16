9137eadfc2698ba56d55995300e48e3f23ac8e85
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-17 09:50:47 +1000

    rewrited task 13

--- Seminar2HW/HW2.cs
+++ Seminar2HW/HW2.cs
@@ -6,18 +6,24 @@
 
 // Задача 13
 
-// int number = new Random().Next(9, 1000);
-// Console.WriteLine(number);
+int number = new Random().Next(1, 10000);
+Console.WriteLine(number);
 
+number = Math.Abs(number);
 
-// if (number<=99)
-// {
-//     Console.WriteLine("Нет третьего числа");
-// }
-// else
-// {
-//     Console.WriteLine(number%10);
-// }
+if (number<100)
+{
+    Console.WriteLine("Нет третьего числа");
+}
+else 
+{
+    while (number>=1000)
+    {
+        number /= 10;
+    }
+    int trd = number%10;
+    Console.WriteLine(trd);
+}
 
 // Задача 15
 // Console.WriteLine("Введите цифру");
