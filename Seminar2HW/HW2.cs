651bcd303e60d74ba984da4be69e0af448666d0a
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-17 15:00:48 +1000

    testing workability

--- Seminar2HW/HW2.cs
+++ Seminar2HW/HW2.cs
@@ -148,19 +148,19 @@
 // }
 
 // Решение Дениса
-Console.Write($"Введите цифру соответствующую дню недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Введите цифру соответствующую дню недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
 
-int result = day % 7;
+// int result = day % 7;
 
-switch (result)
-{
-    case 1: Console.Write($"{day} является понедельником"); break;
-    case 2: Console.Write($"{day} является вторником"); break;
-    case 3: Console.Write($"{day} является средой"); break;
-    case 4: Console.Write($"{day} является четвергом"); break;
-    case 5: Console.Write($"{day} является пятницей"); break;
-    case 6: Console.Write($"{day} является субботним выходным"); break;
-    case 0: Console.Write($"{day} является воскресным выходным"); break;
-    default: break;
-}
+// switch (result)
+// {
+//     case 1: Console.Write($"{day} является понедельником"); break;
+//     case 2: Console.Write($"{day} является вторником"); break;
+//     case 3: Console.Write($"{day} является средой"); break;
+//     case 4: Console.Write($"{day} является четвергом"); break;
+//     case 5: Console.Write($"{day} является пятницей"); break;
+//     case 6: Console.Write($"{day} является субботним выходным"); break;
+//     case 0: Console.Write($"{day} является воскресным выходным"); break;
+//     default: break;
+// }
