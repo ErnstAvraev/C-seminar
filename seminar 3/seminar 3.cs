29434f42df3a0beafbb64155758408a7e5895ab7
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-17 11:49:09 +1000

    first solution

--- seminar 3/seminar 3.cs
+++ seminar 3/seminar 3.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input coordinate x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input coordinate y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("First quarter");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Second quarter");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Third quarter");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("forth quarter");
+}
+else 
+{
+    Console.WriteLine("Can't identify quarter");
+}
