ea006134afba8622a89abedbcd9eafb26cd730b2
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-17 12:05:44 +1000

    added method

--- seminar 3/seminar 3.cs
+++ seminar 3/seminar 3.cs
@@ -1,25 +1,56 @@
-﻿Console.WriteLine("Input coordinate x");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input coordinate y");
-int y = Convert.ToInt32(Console.ReadLine());
+﻿// Напишите программу, которая принимает на вход точки (X и Y)б причем X не равен 0 и Y не равен 0 и выдает к какой четверти принадлежит точка
+// Вводим метод
 
-if (x > 0 && y > 0)
+void printQuarter(int a, int b)
+{
+  if (a > 0 && b > 0)
 {
     Console.WriteLine("First quarter");
 }
-else if (x < 0 && y > 0)
+else if (a < 0 && b > 0)
 {
     Console.WriteLine("Second quarter");
 }
-else if (x < 0 && y < 0)
+else if (a < 0 && b < 0)
 {
     Console.WriteLine("Third quarter");
 }
-else if (x > 0 && y < 0)
+else if (a > 0 && b < 0)
 {
     Console.WriteLine("forth quarter");
 }
 else 
 {
     Console.WriteLine("Can't identify quarter");
+}  
 }
+
+
+Console.WriteLine("Input coordinate x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input coordinate y");
+int y = Convert.ToInt32(Console.ReadLine());
+printQuarter(x, y);
+
+
+// Без метода
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("First quarter");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("Second quarter");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("Third quarter");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("forth quarter");
+// }
+// else 
+// {
+//     Console.WriteLine("Can't identify quarter");
+// }
