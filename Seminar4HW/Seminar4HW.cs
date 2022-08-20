5ac4af4f51f9c6fdce3e03d5657a2f3f494ce778
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-20 20:18:55 +1000

    finished HW + all in comments

--- Seminar4HW/Seminar4HW.cs
+++ Seminar4HW/Seminar4HW.cs
@@ -1,7 +1,7 @@
-﻿// HW 
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿// // HW 
+// // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// // 3, 5 -> 243 (3⁵)
+// // 2, 4 -> 16
 
 
 // void power(double num1, double num2)
@@ -19,39 +19,39 @@
 
 
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
 
 
-int ammountNum(int a)
-{
-    int i = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        i++;
-    }
-    return i;
-}
+// int ammountNum(int a)
+// {
+//     int i = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         i++;
+//     }
+//     return i;
+// }
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int res = num;
-int result = 0;
-int[] arr = new int[ammountNum(num)];
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int res = num;
+// int result = 0;
+// int[] arr = new int[ammountNum(num)];
 
-for (int i = 0; i != arr.Length; i++) // записываем значения в массив
-{
-    res %= 10;
-    num /= 10;
-    arr[i] = res;
-    res = num;
-}
-for (int j = 0; j != arr.Length; j++)
-{
-    result = arr[j] + result;
-}
+// for (int i = 0; i != arr.Length; i++) // записываем значения в массив
+// {
+//     res %= 10;
+//     num /= 10;
+//     arr[i] = res;
+//     res = num;
+// }
+// for (int j = 0; j != arr.Length; j++)
+// {
+//     result = arr[j] + result;
+// }
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
