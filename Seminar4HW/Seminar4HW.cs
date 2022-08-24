b5b92617fab644c11535aa531e88a6ffb8120a36
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-24 14:17:42 +0300

    upgraded my solution of task 25

--- Seminar4HW/Seminar4HW.cs
+++ Seminar4HW/Seminar4HW.cs
@@ -4,42 +4,42 @@
 // // 2, 4 -> 16
 
 
-// void power(double num1, double num2)
-// {
-//     double result = Math.Pow(num1, num2);
-//     Console.WriteLine(result);
-// }
+void power(double num1, double num2)
+{
+    double result = Math.Pow(num1, num2);
+    Console.WriteLine(result);
+}
 
-// Console.Clear ();
-// Console.WriteLine("Введите число A: ");
-// double a = Math.Abs((Convert.ToDouble(Console.ReadLine())));
-// Console.WriteLine("Введите число B: ");
-// double b = Math.Abs(Convert.ToDouble(Console.ReadLine()));
-// power(a,b);
+Console.Clear ();
+Console.WriteLine("Введите число A: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+power(a,b);
 
 
 // Еще одно решение от Елены
 
 
-Console.Clear();
-Console.WriteLine("Введите число: ");
-int A = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите степень: ");
-int B = int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int A = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите степень: ");
+// int B = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
+// Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
 
-int Pow(int num, int rank)
-{
-    if (B == 0) return 1;
+// int Pow(int num, int rank)
+// {
+//     if (B == 0) return 1;
 
-    int result = num;
-    for (int i = 2; i <= rank; i++)
-    {
-        result *= num;
-    }
-    return result;
-}
+//     int result = num;
+//     for (int i = 2; i <= rank; i++)
+//     {
+//         result *= num;
+//     }
+//     return result;
+// }
 
 
 // // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
