8249387dcc160cf289dc0f10ba485f6ae105c396
Ernst Avraev <avraev.ernst@yandex.ru>
2022-08-29 14:03:10 +0300

    seminar 6 finished

--- seminar 6/seminar 6.cs
+++ seminar 6/seminar 6.cs
@@ -78,43 +78,113 @@
 
 // // Еще одно решение
 
-string DecimalToBinary(int input)
-{
-    var result = string.Empty;
-    var devider = 2;
+// string DecimalToBinary(int input)
+// {
+//     var result = string.Empty;
+//     var devider = 2;
 
-    int resultLength = 1;
-    int temp = input;
+//     int resultLength = 1;
+//     int temp = input;
 
-    while (temp / devider > 0)
-    {
-        resultLength++;
-        temp /= devider;
-    }
+//     while (temp / devider > 0)
+//     {
+//         resultLength++;
+//         temp /= devider;
+//     }
 
-    int[] resultArray = new int[resultLength];
+//     int[] resultArray = new int[resultLength];
 
-    int index = resultLength - 1;
+//     int index = resultLength - 1;
 
-    while (index >= 0)
-    {
-        resultArray[index] += input % devider;
-        index--;
-        input /= devider;
-    }
+//     while (index >= 0)
+//     {
+//         resultArray[index] += input % devider;
+//         index--;
+//         input /= devider;
+//     }
 
-    for (int i = 0; i < resultArray.Length; i++)
-    {
-        result += resultArray[i];
-    }
-    return result;
-}
+//     for (int i = 0; i < resultArray.Length; i++)
+//     {
+//         result += resultArray[i];
+//     }
+//     return result;
+// }
+
+// Console.Clear();
+// Console.WriteLine("Введите десятичное число: ");
+// if (!int.TryParse(Console.ReadLine()!, out var number)) // если число выходит за предел допустимых значений
+// {
+//     Console.WriteLine("Все плохо");
+// }
 
 
-Console.WriteLine("Введите десятичное число: ");
-int number = int.Parse(Console.ReadLine()!);
+// var result = DecimalToBinary(number);
 
+// Console.WriteLine("Двоичное представление числа " + number + " есть число " + result);
 
-var result = DecimalToBinary(number);
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа фибонначи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1 
+// Если N = 7 -> 0 1 1 2 3 5 8
 
-Console.WriteLine("Двоичное представление числа " + number + " есть число " + result);
+// void Fibonacci(int N)
+// {
+//     int a1 = 0;
+//     int a2 = 1;
+//     Console.Write($"{a1}, {a2}, ");
+//     for (int a3 = 0; a3 <= N;)
+//     {
+//         a3 = a1 + a2;
+//         a1 = a2;
+//         a2 = a3;
+//         if (a3 > N)
+//         {
+//             break;
+//         }
+//         Console.Write($"{a3}, ");
+//     }
+// }
+// Console.WriteLine("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+
+// Fibonacci(N);
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+// int[] CreateArray() 
+// {
+//     Console.WriteLine("Введите количество элементов массива");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     int[] RandomArray = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.WriteLine($"Введите {i + 1} элемент массива");
+//         RandomArray[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + ", ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] CopyArray(int[] arr)
+// {
+//     int[] result = new int[arr.Length];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         result[i]=arr[i];
+//     }
+//     return result;
+// }
+
+// Console.Clear();
+// int[] myArray = CreateArray();
+// ShowArray(myArray);
+// Console.WriteLine();
+// int [] arrayCopy = CopyArray(myArray);
+// ShowArray(arrayCopy);
