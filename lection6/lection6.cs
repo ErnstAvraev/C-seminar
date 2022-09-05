cb7b4d386cbcdac55690187d08cf8aca5d1cc652
Ernst Avraev <avraev.ernst@yandex.ru>
2022-09-05 21:00:35 +0300

    lection finished

--- lection6/lection6.cs
+++ lection6/lection6.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// Красивый код: какой он?
+//  - гибкий
+//  - расширяемый
+//  - модульный
+//  - поддерживаемый
+//  - документированный 
+
+
+// Random r = new Random();
+
+// Console.CursorVisible = false;
+// while (true)
+// {
+//     Console.SetCursorPosition(
+//         left: r.Next(Console.WindowWidth),
+//         top: r.Next(Console.WindowHeight)
+//     );
+//     Console.Write(r.Next(10));
+//     Thread.Sleep(1000);
+// }
+
+// YAGNI (you aren't gonna need it)
+// процесс и принцип програмирования ПО, при котором в качестве основной цели и/или ценности декларируется отказ от избыточной функциональности, 
+// то есть отказ от добавления функциональности, в которой нет непосредственной надобности
+
+// KISS(Keep it simple, stupid)
+// Принцип, запрещающий использование более сложных средств, чем необходимо.
+// Принцип декларирует простоту системы в качестве основной цели и/или ценности.
+
+// using System.Linq;
+
+// string text = "(1,2) (2,3) (4,5) (6,7)"
+//                 .Replace("(", "")
+//                 .Replace(")", "")
+//                 ;
+// Console.WriteLine(text);
+
+// var data = text.Split(" ")
+//                 .Select(item => item.Split(','))
+//                 .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+//                 .Where(e => e.x % 2 == 0)
+//                 .Select(point => (point.x * 10, point.y + 10))
+//                 .ToArray();
+
+// for (int i = 0; i < data.Length; i++)
+// {
+//     Console.WriteLine(data[i]);
+// }
+
+
