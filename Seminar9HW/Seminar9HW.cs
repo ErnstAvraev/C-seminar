052cec03af556a44989bcb8204aa3ffbaa125df7
Ernst Avraev <avraev.ernst@yandex.ru>
2022-09-08 22:38:00 +0300

    furnished some code

--- Seminar9HW/Seminar9HW.cs
+++ Seminar9HW/Seminar9HW.cs
@@ -52,22 +52,19 @@
 // // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // // m = 2, n = 3 -> A(m,n) = 29
 
-// int Ackerman(int n, int m)
-// {
-//     if (n == 0)
-//         return m + 1;
-//     else
-//     if ((n != 0) && (m == 0))
-//         return Ackerman(n - 1, 1);
-//     else
-//         return Ackerman(n - 1, Ackerman(n, m - 1));
-// }
+int Ackerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else 
+        if ((n != 0) && (m == 0)) return Ackerman(n - 1, 1);
+        else return Ackerman(n - 1, Ackerman(n, m - 1));
+}
 
-// Console.Clear();
-// Console.WriteLine("Ввелите число M: ");
-// int m = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Ввелите число N: ");
-// int n = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.WriteLine("Ввелите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввелите число N: ");
+int n = int.Parse(Console.ReadLine()!);
 
-// if (m < 0 || n < 0) Console.WriteLine("Введите не отрицательные числа!");
-// else Console.WriteLine(Ackerman(m, n));
+if (m < 0 || n < 0) Console.WriteLine("Введите не отрицательные числа!");
+else Console.WriteLine(Ackerman(m, n));
